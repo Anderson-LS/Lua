7b03f3bce2baa1ff45cd527b9068c83225319557
Anderson-LS <quantikus79@gmail.com>
2022-06-26 12:37:38 -0300

    funções apagadas

--- main.lua
+++ main.lua
@@ -1,33 +1,3 @@
-function mover(M)
-     print("Se mova")
-     mover = io.read()
-    
-end
-
-function cima(W)
-    print("se mova")
-    W = io.read()
-
-    if mover() == W == ("W") then
-        return cima == true
-    else
-        return cima == false
-    end
-end
-
-function baixo(S)
-    print("se mova")
-    S = io.read()
-
-    if mover() == S == ("S") then
-        return baixo == true
-    else
-        return baixo == false
-    end
-end
-
-
-
 
 mover()
 
